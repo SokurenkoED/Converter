bf0ec4d57969b34817dd082f3bb4486fa371dd77
SokurenkoED <SokurenkoED@yandex.ru>
2021-09-03 18:12:07 +0300

    Сделал файл main.dat

--- Converter (from xml to dat)/Program.cs
+++ Converter (from xml to dat)/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Converter__from_xml_to_dat_.Files;
+using System;
+using System.IO;
 
 namespace Converter__from_xml_to_dat_
 {
@@ -6,7 +8,22 @@ namespace Converter__from_xml_to_dat_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            #region Создаем папку OldFormat-TIGR
+
+            string DirPath = "OldFormat-TIGR";
+            DirectoryInfo dirInfo = new DirectoryInfo(DirPath);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+            #endregion
+
+            MainXML Main = new MainXML();
+
+
+
         }
     }
 }
