d028eef234107026bf5c6eb583f55dc0665d5491
SokurenkoED <SokurenkoED@yandex.ru>
2021-09-04 21:31:50 +0300

    Записывает все элементы типа 1 в массив.

--- Converter (from xml to dat)/Files/VolidXML.cs
+++ Converter (from xml to dat)/Files/VolidXML.cs
@@ -1,6 +1,7 @@
 ﻿using Converter__from_xml_to_dat_.ElemsOfVolid;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ namespace Converter__from_xml_to_dat_.Files
 {
     class VolidXML
     {
+        IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "." };
+
         /// <summary>
         /// Определили тип элемента
         /// </summary>
@@ -54,6 +57,91 @@ namespace Converter__from_xml_to_dat_.Files
                         Elems Elem = new Elems(); // Создали элемент
 
                         SetTypeOfElem(Elems,ref Elem);
+
+                        if (Elem.Type == "1")
+                        {
+                            Chamb chamb = (Chamb)Elem;
+                            foreach (XElement VOLMLT in Elems.Element("GENERAL_CHAMB").Elements("ELEM_VOLMLT"))
+                            {
+                                XAttribute AttributeValue = VOLMLT.Attribute("Value");
+                                chamb.ELEM_VOLMLT = AttributeValue.Value;
+                            }
+                            foreach (XElement VOLMLT in Elems.Element("GEOM_CHAMB").Elements("CHAMB_VVOL"))
+                            {
+                                XAttribute AttributeValue = VOLMLT.Attribute("Value");
+                                chamb.CHAMB_VVOL = AttributeValue.Value;
+                            }
+                            foreach (XElement VOLMLT in Elems.Element("GEOM_CHAMB").Elements("CHAMB_DZVOL"))
+                            {
+                                XAttribute AttributeValue = VOLMLT.Attribute("Value");
+                                chamb.CHAMB_DZVOL = AttributeValue.Value;
+                            }
+                            foreach (XElement VOLMLT in Elems.Element("GEOM_CHAMB").Elements("CHAMB_FTOVOL"))
+                            {
+                                XAttribute AttributeValue = VOLMLT.Attribute("Value");
+                                chamb.CHAMB_FTOVOL = AttributeValue.Value;
+                            }
+                            if (double.Parse(chamb.CHAMB_FTOVOL, formatter) != 0)
+                            {
+                                foreach (XElement VOLMLT in Elems.Element("STRMAT_CHAMB").Elements("CHAMB_CMVOL"))
+                                {
+                                    XAttribute AttributeValue = VOLMLT.Attribute("Value");
+                                    chamb.CHAMB_CMVOL = AttributeValue.Value;
+                                }
+                                foreach (XElement VOLMLT in Elems.Element("STRMAT_CHAMB").Elements("CHAMB_RMVOL"))
+                                {
+                                    XAttribute AttributeValue = VOLMLT.Attribute("Value");
+                                    chamb.CHAMB_RMVOL = AttributeValue.Value;
+                                }
+                                foreach (XElement VOLMLT in Elems.Element("STRMAT_CHAMB").Elements("CHAMB_DLVOL"))
+                                {
+                                    XAttribute AttributeValue = VOLMLT.Attribute("Value");
+                                    chamb.CHAMB_DLVOL = AttributeValue.Value;
+                                }
+                                foreach (XElement VOLMLT in Elems.Element("STRMAT_CHAMB").Elements("CHAMB_LAMBDA"))
+                                {
+                                    XAttribute AttributeValue = VOLMLT.Attribute("Value");
+                                    chamb.CHAMB_LAMBDA = AttributeValue.Value;
+                                }
+                                foreach (XElement VOLMLT in Elems.Element("STRMAT_CHAMB").Elements("CHAMB_KOCVOL"))
+                                {
+                                    XAttribute AttributeValue = VOLMLT.Attribute("Value");
+                                    chamb.CHAMB_KOCVOL = AttributeValue.Value;
+                                }
+                                foreach (XElement VOLMLT in Elems.Element("STRMAT_CHAMB").Elements("CHAMB_JNM"))
+                                {
+                                    XAttribute AttributeValue = VOLMLT.Attribute("Value");
+                                    chamb.CHAMB_JNM = AttributeValue.Value;
+                                }
+                            }
+                            foreach (XElement VOLMLT in Elems.Element("INITDATA_CHAMB").Elements("CHAMB_PVOL"))
+                            {
+                                XAttribute AttributeValue = VOLMLT.Attribute("Value");
+                                chamb.CHAMB_PVOL = AttributeValue.Value;
+                            }
+                            foreach (XElement VOLMLT in Elems.Descendants().Elements("CHAMB_PSVOL"))
+                            {
+                                XAttribute AttributeValue = VOLMLT.Attribute("Value");
+                                chamb.CHAMB_PSVOL = AttributeValue.Value;
+                            }
+                            foreach (XElement VOLMLT in Elems.Element("INITDATA_CHAMB").Elements("CHAMB_IVOL"))
+                            {
+                                XAttribute AttributeValue = VOLMLT.Attribute("Value");
+                                chamb.CHAMB_IVOL = AttributeValue.Value;
+                            }
+                            foreach (XElement VOLMLT in Elems.Element("INITDATA_CHAMB").Elements("CHAMB_CBOL"))
+                            {
+                                XAttribute AttributeValue = VOLMLT.Attribute("Value");
+                                chamb.CHAMB_CBOL = AttributeValue.Value;
+                            }
+                            foreach (XElement VOLMLT in Elems.Element("INITDATA_CHAMB").Elements("CHAMB_TETVOL"))
+                            {
+                                XAttribute AttributeValue = VOLMLT.Attribute("Value");
+                                chamb.CHAMB_TETVOL = AttributeValue.Value;
+                            }
+                            Elem = chamb;
+
+                        }
 
                         cont.Add(Elem);// Записали элемент в контур
                     }
