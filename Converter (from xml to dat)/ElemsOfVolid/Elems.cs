928910168eb9de06fa54de427ddd18c27dcd38a5
SokurenkoED <SokurenkoED@yandex.ru>
2021-09-04 20:48:29 +0300

    Создаются и контур, и элементы контура, определяется тип контура по названию в comment

---
+++ Converter (from xml to dat)/ElemsOfVolid/Elems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter__from_xml_to_dat_.ElemsOfVolid
+{
+    class Elems
+    {
+        public string Type { get; set; }
+        public string Number { get; set; }
+        public string Description { get; set; }
+    }
+}
