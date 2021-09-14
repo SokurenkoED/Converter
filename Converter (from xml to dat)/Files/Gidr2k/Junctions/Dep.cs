1d76589af18f7411d8e5794ea4188dac06d72ae9
SokurenkoED <SokurenkoED@yandex.ru>
2021-09-14 13:21:04 +0300

    Считывает все виды соединений для файла gidr2k.dat и записывает параметры в массив

---
+++ Converter (from xml to dat)/Files/Gidr2k/Junctions/Dep.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter__from_xml_to_dat_.Files.Gidr2k.Junctions
+{
+    class Dep : Jun
+    {
+        public Dep(string name) : base(name)
+        {
+        }
+
+        public string JUN_AJNMLT { get; set; }
+        public string JUN_HJ1 { get; set; }
+        public string JUN_HJ2 { get; set; }
+        public string JUN_JJNPAR { get; set; }
+        public string JUN_JJNT { get; set; }
+        public string JUN_KCI2KJ { get; set; }
+
+        public List<string> JUN_KC2KT_ARG = new List<string>();
+        public List<string> JUN_KC2KT = new List<string>();
+    }
+}
