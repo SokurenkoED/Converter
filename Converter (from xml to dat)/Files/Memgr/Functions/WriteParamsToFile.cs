e0be9968712fb441bf48b98ed9a498eded4dfcca
Евгений Сокуренко <sokurenkoed@yandex.ru>
2023-08-01 18:50:45 +0300

    Изменил memgr

--- Converter (from xml to dat)/Files/Memgr/Functions/WriteParamsToFile.cs
+++ Converter (from xml to dat)/Files/Memgr/Functions/WriteParamsToFile.cs
@@ -11,8 +11,6 @@ namespace Converter__from_xml_to_dat_.Files.Memgr.Functions
         {
             using (StreamWriter sw = new StreamWriter("OldFormat-TIGR/memgr.dat", false, Encoding.Default))
             {
-                sw.WriteLine(" 0 0 0");
-                sw.WriteLine(" 0");
                 sw.WriteLine(" YES YES");
                 sw.WriteLine(" 0");
                 sw.WriteLine(" AFULLI AFULLM");
