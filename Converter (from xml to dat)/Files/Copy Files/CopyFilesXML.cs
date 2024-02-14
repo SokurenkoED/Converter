40f442f20e1662a52a4afa511f4096c28d074c47
Евгений Сокуренко <sokurenkoed@yandex.ru>
2024-02-14 17:59:53 +0300

    Проверил ЛАЭС

--- Converter (from xml to dat)/Files/Copy Files/CopyFilesXML.cs
+++ Converter (from xml to dat)/Files/Copy Files/CopyFilesXML.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.IO.Enumeration;
+using System.Linq;
 using System.Text;
 
 namespace Converter__from_xml_to_dat_.Files.Copy_Files
@@ -56,6 +57,18 @@ namespace Converter__from_xml_to_dat_.Files.Copy_Files
             }
         }
 
+        public void Copy_file_for_extantion(string ext, string NewName)
+        {
+            string[] FileName = Directory.GetFiles("./", ext, SearchOption.TopDirectoryOnly);
+
+            if (FileName.Length != 0)
+            {
+                File.Copy(FileName[0], $"OldFormat-TIGR/{NewName}", true);
+            }
+           
+
+        }
+
         public CopyFilesXML()
         {
             Copy_file_from_SIT_to_TIGR_Dir("Bipr7.dat");
@@ -66,9 +79,21 @@ namespace Converter__from_xml_to_dat_.Files.Copy_Files
 
             Copy_file_from_SIT_to_TIGR_Dir("list");
 
+            Copy_file_from_SIT_to_TIGR_Dir("list");
+
             Create_directory($"OldFormat-TIGR/SVRK");
 
             Create_files();
+
+            Copy_file_for_extantion("*.drm", "rest.drm");
+
+            Copy_file_for_extantion("*.all", "rest.all");
+
+            Copy_file_for_extantion("*.tfc", "rest.tfc");
+
+            Copy_file_for_extantion("*.res7", "PRO.res");
+
+            Copy_file_for_extantion("*.res7D", "B7D_14.res");
 
 
         }
